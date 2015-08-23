78cf74cf3a7761c9a817d70c3b7a06d9988590a9
balanca <baptiste.balanca@gmail.com>
2015-08-23 16:38:16 +0200

    Update of the Brain Metabolism function CMRGluc.cal and the subsequent documentation

--- R/CMRGluc.R
+++ R/CMRGluc.R
@@ -1,13 +1,14 @@
 CMRGluc <- function (CMRG, # to be calculated
                      Gbrain, # micromol/mL (from my data)
                      Vd=0.77, # glucose brai space difusion - ml/g
-                     Kt=13.4, # apparent maximal transport rate
-                     Tmax=1.35, 
-                     Gplasma=6) #mmol/L
+                     Kt=1.4, # apparent maximal transport rate
+                     Tmax=1.27, 
+                     Gplasma=7.3) #mmol/L
 {
 
-  (Vd*( ( ( (Tmax/CMRG)-1)*Gplasma - Kt ) / ( (Tmax/CMRG) + 1) ) ) - Gbrain
+  (Vd*( ( ( (Tmax/CMRG)-1)*Gplasma - Kt ) / ((Tmax/CMRG) + 1) ) ) - Gbrain
 }
+
 
 #' CMRGluc calculation
 #' 
@@ -19,29 +20,32 @@ CMRGluc <- function (CMRG, # to be calculated
 #' 
 #' @param Gbrain : a vector/numeric value of extracellular glucose concentration in mmol/L, time decay
 #' @param Vd : glucose brain space diffusion (default is 0.77 ml/g)
-#' @param Kt : glucose apparent maximal transport rate (default is 13.4 mmol/L)
-#' @param Tmax : the apparent maximal transport rate (default is 1.35 micormol/g/min)
+#' @param Kt : glucose apparent maximal transport rate (default is 1.4 mmol/L)
+#' @param Tmax : the apparent maximal transport rate (default is 1.27 micormol/g/min)
 #' @param Gplasma : plasma glucose concentration (default=6mmol/L)
-#' @references Du et al JCBFM 2012 32(9)
-#' @references Gruetter et al J Neurochem 1998 70(1)
+#' @references Morgenthaler et al. Neurochem Int 2006, 48
+#' @references Ori et al. Anesthesiology 1986, 65(2)
+#' @references Duarte et al. Front in Neuroenrgetics 2009, 1
+#' @references Gruetter et al J Neurochem 1998, 70(1)
 #' @return CMRGlucose : numeric value of Cerebral metabolic rate of glucose in micromol/g/min
 #' @export
 
-CMRGluc.calc<- function (Gbrain, # micromol/mL (from my data)
+CMRGluc.calc<- function (Gbrain, # mmol/L (from my data)
                         Vd=0.77, # glucose brai space difusion - ml/g
-                        Kt=13.4, # apparent maximal transport rate
-                        Tmax=1.35,
-                        Gplasma=6) #mmol/L
+                        Kt=1.4, # apparent maximal transport rate
+                        Tmax=1.27,
+                        Gplasma=7.3) #mmol/L
   { 
  
     CMR<-NA
     X11()
     for (i in 1:length(Gbrain)) {
-      if (tail(curve(CMRGluc(x,Gbrain[i],Vd,Kt,Tmax,Gplasma),from=0,to=1000,n=1001)[[2]],n=1)=="NaN"){
-        maxindex<-which(curve(CMRGluc(x,Gbrain[i],Vd,Kt,Tmax,Gplasma),from=0,to=1000,n=1001)[[2]]=="NaN")[1]-2
+      if (tail(curve(CMRGluc(x,Gbrain[i],Vd,Kt,Tmax,Gplasma),from=0.0001,to=10,n=10000)[[2]],n=1)=="NaN"){
+        maxindex<-which(curve(CMRGluc(x,Gbrain[i],Vd,Kt,Tmax,Gplasma),from=0.0001,to=10,n=10000)[[2]]=="NaN")[1]-2
       }
-      else {maxindex<-1000}
-      CMR[i] <- uniroot(CMRGluc, c(0, maxindex),Gbrain[i],Vd,Kt,Tmax,Gplasma)$root
+      else {maxindex<-10000}
+      
+      CMR[i] <- uniroot(CMRGluc, c(0.0001,maxindex/1000),Gbrain[i],Vd,Kt,Tmax,Gplasma)$root
     }
     dev.off()
 
